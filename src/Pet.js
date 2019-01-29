e6de8efcff68417fe4e45dcaed54191898237940
Mitchell <mitchell.blom@getbuilt.com>
2019-01-28 20:57:46 -0600

    added routing from results to pets and back to homepage

--- src/Pet.js
+++ src/Pet.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 class Pet extends React.Component {
   render() {
-    const { name, animal, breed, media, location } = this.props;
+    const { name, animal, breed, media, location, id } = this.props;
   
     let photos = []
 
@@ -11,17 +12,19 @@ class Pet extends React.Component {
     }
 
     return (
-      <div className="pet">
-        <div className="image-container">
-          <img src={photos[0].value} alt={name}></img>
+      <Link to={`/details/${id}`} className="pet">
+        <div className="pet">
+          <div className="image-container">
+            <img src={photos[0].value} alt={name}></img>
+          </div>
+          <div className="info">
+            <h1>{name}</h1>
+            <h2>
+              {animal} - {breed} - {location}
+            </h2>
+          </div>
         </div>
-        <div className="info">
-          <h1>{name}</h1>
-          <h2>
-            {animal} - {breed} - {location}
-          </h2>
-        </div>
-      </div>
+      </Link>
     )
   }
 }
